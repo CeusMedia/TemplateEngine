4d11f21039e76a8c85c96a546377d123cae72732
kriss0r <kriss@ceusmedia.de>
2023-06-18 04:18:10 +0200

    Update copyright year

--- src/Filter/Rot13.php
+++ src/Filter/Rot13.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /**
  *	ROT13 Filter.
  *
- *	Copyright (c) 2011-2022 Christian Würker (ceusmedia.de)
+ *	Copyright (c) 2011-2023 Christian Würker (ceusmedia.de)
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@ declare(strict_types=1);
  *	@category		Library
  *	@package		CeusMedia_TemplateEngine_Filter
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
- *	@copyright		2011-2022 Christian Würker
+ *	@copyright		2011-2023 Christian Würker
  *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/TemplateEngine
  */
@@ -36,7 +36,7 @@ use CeusMedia\TemplateEngine\FilterAbstract;
  *	@category		Library
  *	@package		CeusMedia_TemplateEngine_Filter
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
- *	@copyright		2011-2022 Christian Würker
+ *	@copyright		2011-2023 Christian Würker
  *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/TemplateEngine
  */
--- src/FilterInterface.php
+++ src/FilterInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /**
  *	Interface for template engine filters.
  *
- *	Copyright (c) 2011-2022 Christian Würker (ceusmedia.de)
+ *	Copyright (c) 2011-2023 Christian Würker (ceusmedia.de)
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ declare(strict_types=1);
  *	@package		CeusMedia_TemplateEngine
  *	@author			David Seebacher <dseebacher@gmail.com>
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
- *	@copyright		2007-2022 Christian Würker
+ *	@copyright		2007-2023 Christian Würker
  *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/TemplateEngine
  */
@@ -36,7 +36,7 @@ namespace CeusMedia\TemplateEngine;
  *	@package		CeusMedia_TemplateEngine
  *	@author			David Seebacher <dseebacher@gmail.com>
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
- *	@copyright		2007-2022 Christian Würker
+ *	@copyright		2007-2023 Christian Würker
  *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/TemplateEngine
  */
